e8178344231296ed6bbe8c76097e867f93e78bbf
Lucca Benedetti <luccabenedetti@protonmail.com>
2023-03-06 22:27:58 -0300

    Changes to be committed: 	modified:   polybarWeather.py
    updated functions

--- polybarWeather.py
+++ polybarWeather.py
@@ -5,23 +5,29 @@ import urllib.parse
 def getWeatherByLatLon(lat, lon):
     url = 'https://weather.contrateumdev.com.br/api/weather?lat=' + str(lat) + '&lon=' + str(lon)
     r = requests.get(url)
-    return r.json()
+    try:
+        return r.json()
+    except ValueError:
+        return {}
 
 def getWeatherByCityState(city, state):
     url = 'https://weather.contrateumdev.com.br/api/weather/city/?city=' + city + ',' + state
     r = requests.get(url)
-    return r.json() 
+    try:
+        return r.json()
+    except ValueError:
+        return {} 
 
 method = 'lat/lon' # change to city/state if you want to use it this way
 
 if method == 'lat/lon':
-    with open('your_path/config.json') as file:
+    with open('path of config.json') as file:
         config = json.loads(file.read())
     latitude = config["lat"]
     longitude = config["lon"]
     r = getWeatherByLatLon(latitude, longitude)
 elif method == 'city/state':
-    with open('your_path/config.json') as file:
+    with open('path of config.json') as file:
         config = json.loads(file.read())
     city = urllib.parse.quote(config["city"])
     state = urllib.parse.quote(config["state"])
@@ -29,10 +35,13 @@ elif method == 'city/state':
 else:
     print('invalid method')
 
-with open('your_path/weatherIcons.json') as file:
+with open('path of weatherIcons.json') as file:
     d = json.loads(file.read())
 
-temp = int(r["main"]["temp"])
-weather = r['weather'][0]['icon']
-weatherIcon = d[f'{weather}']
-print(weatherIcon + ' ' + str(temp) + '°C')
+if not r:
+    print("")
+else:
+    temp = int(r["main"]["temp"])
+    weather = r['weather'][0]['icon']
+    weatherIcon = d[f'{weather}']
+    print(weatherIcon + ' ' + str(temp) + '°C')
